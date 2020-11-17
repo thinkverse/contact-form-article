a40ab402bd23bde6f72acbe6de9781399280b4d0
thinkverse <hallberg.kim@gmail.com>
2020-11-17 17:19:51 +0100

    Update ContactForm.php Add component properties

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -6,6 +6,42 @@ use Livewire\Component;
 
 class ContactForm extends Component
 {
+    /**
+     * The name person uses
+     *
+     * @var string $name
+     */
+    public string $name = '';
+
+    /**
+     * The email person uses
+     *
+     * @var string $email
+     */
+    public string $email = '';
+
+    /**
+     * The phone number person uses
+     *
+     * @var string $phone
+     */
+    public string $phone = '';
+
+    /**
+     * The attached message
+     *
+     * @var string $message
+     */
+    public string $message = '';
+
+    /**
+     * The person's preferred
+     * way of contact
+     *
+     * @var int $preferred
+     */
+    public int $preferred = 0;
+
     public function render()
     {
         return view('livewire.contact-form');
